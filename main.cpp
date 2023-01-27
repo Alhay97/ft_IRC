2ad212210cf297adef60ac2134c1028feaeefaa4
alhai alhameli <Alhay.7@gmail.com>
2023-01-27 18:11:44 +0400

    finished with arguments

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
-int main ()
+int main(int counter, char **str)
 {
-	
+	try
+	{
+		if(counter == 3)
+		{
+			std::cout << str[1] << std::endl;
+			std::cout << str[2] << std::endl;
+		}
+		else
+			throw std::invalid_argument("invalid arguments");
+	}
+	catch(const std::invalid_argument& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	return 0;
 }
+
+
