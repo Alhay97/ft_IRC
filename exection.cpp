34056277f07525ea04cdf80800575caf80234f12
alhai alhameli <Alhay.7@gmail.com>
2023-01-28 17:16:38 +0400

    continue with parsing

--- exection.cpp
+++ exection.cpp
@@ -2,10 +2,43 @@
 
 
 // create an atoi for cpp 98 standard for the port
-
+	//should be less than 65535 and greater than 0 (done)
+	//should not be part of reserverd ports
+	//
 //
 
 
+int stringToInt(std::string str)
+{
+	int num = 0;
+	int i  = 0;
+	int size = str.length();
+	while (i < size)
+	{
+        num = num * 10 + (str[i] - '0');
+        i++;
+    }
+    return num;
+}
+
+
+int portChecker(int port)
+{
+	try
+	{
+		if(port <= 65535 && port >= 0)
+			return (port);
+		else
+			throw std::invalid_argument("not in port range");
+	}
+
+	catch(const std::invalid_argument& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	return (0);
+}
 
 int main(int counter, char **str)
 {
@@ -13,6 +46,7 @@ int main(int counter, char **str)
 	{
 		if(counter == 3)
 		{
+			portChecker(stringToInt(str[1]));
 			std::cout << str[1] << std::endl;
 			std::cout << str[2] << std::endl;
 		}
@@ -25,4 +59,7 @@ int main(int counter, char **str)
 	}
 	return 0;
 }
+
+
+
 
