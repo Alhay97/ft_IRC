4279623d12130d456a7953a4f5476c1901002556
alhai alhameli <Alhay.7@gmail.com>
2023-06-11 01:04:22 +0400

    finish each function

--- server.cpp
+++ server.cpp
@@ -1,22 +1,3 @@
 #include <iostream>
 #include "server.hpp"
 
-Server::Server()
-{
-	// default constructor
-}
-Server::Server()
-{
-	//constructor
-}
-Server::Server(Server &copy){
-
-}
-Server &Server::operator=(Server &source){
-
-}
-
-Server::~Server()
-{
-	// default constructor
-}
--- server.hpp
+++ server.hpp
@@ -3,18 +3,39 @@
 
 #include <iostream>
 #include <string>
+#include <fcntl.h>
+#include <poll.h>
 
 class Server
 {
 	private:
+		std::string host;
+		std::string port; // the port to listen to
+		std::string password; // the password to use
 
 	public:
-	Server();// default constructor
-	Server();//constructor
-	Server(Server &copy);
-	Server &operator=(Server &source);
-	~Server();
+		enum status
+		{
+			OFFLINE,
+			ONLINE,
+			CLOSED,
+		}_status;
+		int _fd;
+		std::vector<pollfd> _pfds;
 
+	public:
+		Server(std::string host, std::string port, std::string password);
+		~Server();
+		void close_server();
+		void run();
+		bool is_running();
+		bool has_password();
+		std::string get_password();
+		std::string get_server_name();
 };
+
+
+bool validate_arguments(int argc, char **argv);
+
 
 #endif
